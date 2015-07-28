11368e179435811f89568c90bd99b9d7b9b34ad2
Marcin Czachurski <mczachurski@icloud.com>
2015-07-28 19:41:36 +0200

    Initial Commit

---
+++ Remotly/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Remotly
+//
+//  Created by Marcin Czachurski on 28.07.2015.
+//  Copyright (c) 2015 SunLine Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
