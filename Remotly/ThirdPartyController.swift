ae724411313ef8a5e2c15ee472a1a7c4bd875dd3
Marcin Czachurski <mczachurski@icloud.com>
2015-08-30 13:05:30 +0200

    Add information about SwiftLoader: https://github.com/leoru/SwiftLoader.

--- Remotly/ThirdPartyController.swift
+++ Remotly/ThirdPartyController.swift
@@ -30,14 +30,16 @@ class ThirdPartyController: UITableViewController
     {
         switch(indexPath.row)
         {
-        case 0:
-            gotoWebsite("https://icons8.com")
-        case 1:
-            gotoWebsite("https://github.com/SwiftyJSON/SwiftyJSON")
-        case 2:
-            gotoWebsite("https://github.com/bryx-inc/BRYXBanner")
-        default:
-            break;
+            case 0:
+                gotoWebsite("https://icons8.com")
+            case 1:
+                gotoWebsite("https://github.com/SwiftyJSON/SwiftyJSON")
+            case 2:
+                gotoWebsite("https://github.com/bryx-inc/BRYXBanner")
+            case 3:
+                gotoWebsite("https://github.com/leoru/SwiftLoader")
+            default:
+                break;
         }
     }
     
